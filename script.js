529c3c46585e28072dcd707bc9f63a6109498846
wagnerrafa <wagner_rafa@hotmail.com.br>
2020-07-09 14:58:49 -0300

    pegar imagem e nome, e card para exibicao

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 const tStamp =  '1594244037319';
 const publicKey = 'ca318e6cf87496125a923886913f04db';
-const privateKey = 'b5dd7d6f4d2b4480fdd4e39fac17314286334028';
-console.log(tStamp);
-const maxCharacters = 1500;
-const offset = Math.floor((Math.random() * maxCharacters) + 1);
-
-const juntarKey = tStamp + privateKey + publicKey;
 const criarMd5 = 'a56edaf91c07ef92a38c3369c3fbe89d';
+heroiAleatorio();
 
 function heroiAleatorio() {
 
-    const offset = Math.floor((Math.random() * maxCharacters) + 1);
+    const offset = Math.floor((Math.random() * 1500) + 1);
     console.log(offset);
-    
-    const url = "http://gateway.marvel.com/v1/public/characters?limit=9&offset="+offset+"&ts="+tStamp+"&apikey="+publicKey+"&hash="+criarMd5;
+    const url = "http://gateway.marvel.com/v1/public/characters?limit=4&offset="+offset+"&ts="+tStamp+"&apikey="+publicKey+"&hash="+criarMd5;
 
     var http = new XMLHttpRequest();
     http.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var data = JSON.parse(this.responseText);
-            //getImages(data); criar a funcao para obter a imagem
+            pegarImagem(data);
             console.log("aeeeee");
         }
     };
@@ -27,4 +21,17 @@ function heroiAleatorio() {
     http.send();
 }
 
-heroiAleatorio();
+function pegarImagem(dados){ //dados recebidos pelo xml
+
+    console.log(dados["data"]["results"]);
+    var dadosPersonagens = dados["data"]["results"];
+
+    for (var i = 0; i < 4; i++) {    
+        
+        var resultado = dadosPersonagens[i];
+        card = document.querySelector("#c"+i+"");
+        card.querySelector("#i"+i+"").src = resultado["thumbnail"]["path"] +"."+ resultado["thumbnail"]["extension"]; //endereço da imagem
+        card.querySelector("#n"+i+"").textContent = "Nome: "+resultado["name"]; //return do nome
+    }
+
+}
